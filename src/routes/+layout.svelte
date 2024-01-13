99a741eb1aa224d89ec26d5baa70d07e41480c47
Alex Hadley <alexhad6@users.noreply.github.com>
2024-01-12 22:17:17 -0800

    Add initial SVG element and 404 page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+	<title>Temperature Blanket</title>
+</svelte:head>
+
+<h1>Temperature Blanket</h1>
+
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Temperature Blanket</h1>
+<script lang="ts">
+	const boxesWidth = 6;
+	const boxesHeight = 5;
+	const boxWidth = 10;
+	const boxHeight = 7;
+	const boxGap = 1;
+	const svgWidth = boxesWidth * (boxWidth + boxGap) - boxGap;
+	const svgHeight = boxesHeight * (boxHeight + boxGap) - boxGap;
+	const numColors = (1 << 24) - 1;
+</script>
+
 <p>🏗️ Under construction 🚧</p>
+
+<svg viewBox="0 0 {svgWidth} {svgHeight}">
+	{#each Array(boxesWidth) as _, boxX}
+		{#each Array(boxesHeight) as _, boxY}
+			<rect
+				x={boxX * (boxWidth + boxGap)}
+				y={boxY * (boxHeight + boxGap)}
+				width={boxWidth}
+				height={boxHeight}
+				fill="#{Math.floor(Math.random() * numColors).toString(16)}"
+			/>
+		{/each}
+	{/each}
+</svg>
